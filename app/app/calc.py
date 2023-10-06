b806d4a9959f448e4f3920ce7557331a3422476f
asim-noor <noor@1-to-n.com>
2023-10-06 16:42:18 +0500

    Wait for db implenented

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+Calculator
+"""
+
+
+def add(x, y):
+    """
+    adds two number
+    """
+
+    return x + y
+
+
+def subtract(x, y):
+    """subtract x from y and return"""
+
+    return y - x
---
+++ app/app/test.py
@@ -0,0 +1,23 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class ClacTest(SimpleTestCase):
+    """
+    test the calc module.
+    """
+
+    def test_add_number(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting the numbers"""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res, 5)
